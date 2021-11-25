ebb6176de4047e0d412112f6b3c7ba6362a7f420
ArtemLytvynenkoa <chpart355@gmail.com>
2021-11-25 21:39:43 +0200

    adds changes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,9 @@
 import Throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const feedbackFormData = {};
-   
+let feedbackFormData = {};
 
+    
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', Throttle(onFormInput, 500));
 
@@ -11,30 +11,31 @@ repairFormsData();
 
 function onFormInput(evt) {
     feedbackFormData[evt.target.name] = evt.target.value;
-
     localStorage.setItem('feedback-form-state', JSON.stringify(feedbackFormData));
 };
 
 function onFormSubmit(evt) {
     evt.preventDefault();
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
     evt.currentTarget.reset();
     localStorage.removeItem('feedback-form-state');
+    feedbackFormData = {};
 };
 
 function repairFormsData() {
     const saveFeedbackFormData = localStorage.getItem('feedback-form-state');
     const parsedFeedbackFormData = JSON.parse(saveFeedbackFormData);
-    
     if (parsedFeedbackFormData) {
         form.email.value = parsedFeedbackFormData.email;
         form.message.value = parsedFeedbackFormData.message;
+
+        if (!parsedFeedbackFormData.email) {
+            form.email.value = '';
+        };
+        if (!parsedFeedbackFormData.message) {
+            form.message.value = '';
+        }
+        
     };
-    if (parsedFeedbackFormData.email === undefined || parsedFeedbackFormData.message) {
-        form.email.value = '';
-        form.message.value = parsedFeedbackFormData.message;
-    };
-    if (parsedFeedbackFormData.email || parsedFeedbackFormData.message === undefined) {
-        form.email.value = parsedFeedbackFormData.email;
-        form.message.value = '';
-    }
+    feedbackFormData = { ...parsedFeedbackFormData };
 }
